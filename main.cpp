5f13b51c5954439971269f4908b896291a2ae9e0
Enperry <103221656+Adam1000-code@users.noreply.github.com>
2024-01-01 20:54:27 -0500

    goodbye comments... for now

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-// this is going to be a flappy bird game bc raylib is hard to use for anything else
 #include <raylib.h>
 
-// this defines the screen. touch and you will die
 const int screenWidth = 1280;
 const int screenHeight = 720;
 
@@ -11,26 +9,22 @@ const double gameGravity = 2;
 
 void Player(Texture2D player, int y, double gravity)
 {
-    DrawTexture(player, 0, y, WHITE);
+    DrawTexture(player, 0 / 2, y, WHITE);
 }
 
 int main()
 {
     playerY = screenHeight;
-    // but why aren't the screen width and height defined IN the main function?
-    // don't ask why. js go with the flow
     InitWindow(screenWidth, screenHeight, "Flappy Boi");
     
-    // tha main gooberoo. delete this and I delete you
     Texture2D playerSprite = LoadTexture("player.png");
     
-    // the target FPS. change it and I will murder you
     SetTargetFPS(60);
     
     while(!WindowShouldClose())
     {
         BeginDrawing();
-            Player(playerSprite, playerY / 2, gameGravity);
+            Player(playerSprite, playerY / 1.8, gameGravity);
         EndDrawing();
     }
     
